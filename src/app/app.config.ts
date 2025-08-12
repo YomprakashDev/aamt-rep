8c42cdc184828d579de7a2107f2c1371c369b6fc
Om Prakash <omprakashdevmail@gmail.com>
2025-08-12 15:56:44 +0530

    7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts';
-
-import { environment } from './environmental';
 import { routes } from './app.routes';
+// ✅ Import AngularFire initialization functions
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChfno8-A3FUs7ZmfYjz4a3aV_QVYtoVgk",
+  authDomain: "aamt-login-test.firebaseapp.com",
+  projectId: "aamt-login-test",
+  storageBucket: "aamt-login-test.firebasestorage.app",
+  messagingSenderId: "747701155359",
+  appId: "1:747701155359:web:c39e356aef28a1318d472a",
+  measurementId: "G-9MXPBQ8G2L"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,8 +25,11 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
      provideEchartsCore({ echarts }),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+
+])
   ]
 };
 ``
--- src/app/environmental.ts
+++ src/app/environmental.ts
@@ -1,12 +1,12 @@
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyChfno8-A3FUs7ZmfYjz4a3aV_QVYtoVgk",
-    authDomain: "aamt-login-test.firebaseapp.com",
-    projectId: "aamt-login-test",
-    storageBucket: "aamt-login-test.firebasestorage.app",
-    messagingSenderId: "747701155359",
-    appId: "1:747701155359:web:c39e356aef28a1318d472a",
-    measurementId: "G-9MXPBQ8G2L"
-  }
-};
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyChfno8-A3FUs7ZmfYjz4a3aV_QVYtoVgk",
+//     authDomain: "aamt-login-test.firebaseapp.com",
+//     projectId: "aamt-login-test",
+//     storageBucket: "aamt-login-test.firebasestorage.app",
+//     messagingSenderId: "747701155359",
+//     appId: "1:747701155359:web:c39e356aef28a1318d472a",
+//     measurementId: "G-9MXPBQ8G2L"
+//   }
+// };
